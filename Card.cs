f9f174fdf531e7438391b3bb5a291283bec4c201
Ty Budvarson <tygunnarbud@gmail.com>
2022-10-18 15:15:22 -0600

    I copy pasted waht was already there

--- Card.cs
+++ Card.cs
@@ -7,9 +7,9 @@ namespace Unit03.Game
     ///Card is for drawing the first card from the deck
     public class Card
     {
-        private int cardnum = 0;
+        public int cardnum = 0;
 
-        private void Draw()
+        public void Draw()
         {
             Random rnd = new Random();
             int cardnum = rnd.Next(1,13);
